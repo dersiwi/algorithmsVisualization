10ad35ce19662b2e40c678c9525cf3e8427116e0
Simon Wilhelm Schübel <siwischbl@gmail.com>
2022-12-15 15:35:01 +0100

    Great amounts of cleaned up code, added multiple drawing colors, got started on Dijstra, added weights to the graph and how to calculate them based on the square colors.

--- scripts/shortestPaths2.js
+++ scripts/shortestPaths2.js
@@ -1,7 +1,19 @@
 //---------------------------------------------colors
 
 const canvasBackgroundColor = '#816797';
-const defaultNodeColor = 'white';
+const defaultNodeColor = '#FFFFFF';
+
+const startPointColor = '#0000FF';
+const finishPointColor = '#FF0000';
+const squareColor = defaultNodeColor;
+const obstacleColor = '#000000';
+const pathColor = '#FFFF00';
+const visited = '#FFA500';
+const queuedColor = '#808080';
+
+var drawingColor = obstacleColor;
+
+const msBetweenDraws = 100;
 
 const defaultSquareWidth = 3;
 var squareSpacing = 1;
@@ -10,17 +22,17 @@ var squareWidth = defaultSquareWidth*2;
 //---------------------------------------------setup global variables 
 const maxCanvasDimension = 900;
 const canvas = document.querySelector('#canvas');
+const originalCanvasHeight = canvas.height;
+const originalCanvasWidth = canvas.width;
 
-canvas.width = getDesiredCanvasDimension(canvas.width);
-canvas.height = getDesiredCanvasDimension(canvas.height);
+canvas.width = getDesiredCanvasDimension(originalCanvasWidth);
+canvas.height = getDesiredCanvasDimension(originalCanvasHeight);
 
 
 const ctx = canvas.getContext('2d');
-//canvas.addEventListener("mousedown", mouseDownOnCanvas, false);
+canvas.addEventListener("mousedown", mouseDownOnCanvas, false);
 
 
-//alert(squaresPerRow);
-//alert(amountSquareRows);
 
 function getDesiredCanvasDimension(current){
     console.log("calculating optimal canvas size");
@@ -40,22 +52,57 @@ function getDesiredCanvasDimension(current){
 
 
 //----------------------------------------define all classes 
-
+    //convert an rgb-object into its perceived brightnesss
+function rgbToLuma(rgb){
+        //more accurate formula : Y = 0.2126 R + 0.7152 G + 0.0722 B
+        return (2*rgb.r+ rgb.b+3*rgb.g)/6;
+    }
 
 
 class Node {
-    constructor(x, y, width=squareWidth, color=defaultNodeColor){
+    constructor(x, y, index, width=squareWidth, color=defaultNodeColor){
         this.x = x;
         this.y = y;
+        this.index = index;
         this.width = width;
         this.color = color;
         this.reachableNodes = [];
         this.parent = null;
     }
 
-    addNeighbour(node){
-        this.reachableNodes.push(node);
+    setColor(color){
+        this.color = color;
     }
+
+    addNeighbour(node){
+        this.reachableNodes.push([node, this.calculateWeight(this, node)]);       
+    }
+
+    
+
+    //calculates the weight of the edge between node1 and node 2 based on their color.
+    calculateWeight(node1, node2) {
+        console.log("Calculating weight");
+        //the weight between two nodes is their difference in brightness 
+        var node1Brightness = rgbToLuma(node1.getColorAsRGB());
+        var node2Brightness = rgbToLuma(node2.getColorAsRGB());
+        var weightOfNodes = (255 + (node1Brightness - node2Brightness)) / 2;
+        console.log(weightOfNodes);
+        return weightOfNodes;
+    }
+
+
+
+    getColorAsRGB(){
+        //source  : https://stackoverflow./questions/5623838/rgb-to-hex-and-hex-to-rgb
+        var result = /^#?([a-f\d]{2})([a-f\dcom]{2})([a-f\d]{2})$/i.exec(this.color);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+
 
     getReachableNodes(){
         return this.reachableNodes;
@@ -66,9 +113,72 @@ class Node {
     }
 
     draw(){
-        console.log(this.color);
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.width);
+    }
+};
+
+
+class PriorityQueue{
+
+    constructor(){
+        this.items = []; 
+    }
+
+    //this is absoloute garbage! - it should be implemented using a binary tree
+    enqueue(element, priority)
+    {
+        // creating object from queue element
+        var qElement = new PriorityQueueElement(element, priority);
+        var contain = false;
+    
+        // iterating through the entire
+        // item array to add element at the
+        // correct location of the Queue
+        for (var i = 0; i < this.items.length; i++) {
+            if (this.items[i].priority > qElement.priority) {
+                // Once the correct location is found it is
+                // enqueued
+                this.items.splice(i, 0, qElement);
+                contain = true;
+                break;
+            }
+        }
+    // if the element have the highest priority
+    // it is added at the end of the queue
+        if (!contain) {
+            this.items.push(qElement);
+        }
+    }
+
+    deleteElment(element)
+    {
+        for (var i = 0; i < this.items.length;i++){
+            if (this.items[i].element.index == element.index){
+                this.items.splice(i, 1);
+            }
+        }
+    }
+
+    //completely disregarding runtime at this point... 
+    changePriority(element, newPriority){
+        this.deleteElement(element);
+        this.enqueue(element, newPriority);
+    }
+
+    getMinPrio(){
+        return this.items.shift();
+    }
+
+    isEmpty(){
+        return this.items.length == 0;
+    }
+}
+
+class PriorityQueueElement{
+    constructor(item, priority){
+        this.item = item;
+        this.priority = priority;
     }
 }
 
@@ -83,13 +193,19 @@ class Coordinate{
         this.x = gridIndexX;
         this.y = gridIndexY;
     }
-}
+
+    getX(){return this.x;}
+    getY(){return this.y;}
+};
 
 //---------------------------------------variables for algorithm
 
 var grid = [];
 var startingPoint = new Coordinate(-1, -1);
 var finishingPoint = new Coordinate(-1, -1);
+
+var startingNode = null;
+var goalNode = null;
 
 
 //---------------------------------------setup of the grid
@@ -101,38 +217,44 @@ function setupGrid(){
     var squaresPerRow =  Math.floor((canvas.width - squareSpacing) / (squareWidth + squareSpacing));
     var amountSquareRows = Math.floor((canvas.height - squareSpacing) / (squareWidth + squareSpacing));
 
-
+    var indexCounter = 0;
     for (rows = 0; rows < amountSquareRows; rows++){
         grid[rows] = [];
         for (columns = 0; columns < squaresPerRow; columns++){
             grid[rows][columns] = new Node(
                 x = squareSpacing + columns * (squareWidth + squareSpacing), 
                 y = squareSpacing + rows * (squareWidth + squareSpacing), 
+                index = indexCounter,
                 width=squareWidth, 
                 color=defaultNodeColor);
+            indexCounter++;
         }
     }
+}
 
+function setupGraph(){
+    console.log("setting up graph");
     //add all the reachable neighbours -- essentially build the graph
-    for (rows = 0; rows < amountSquareRows; rows++){
-        for (columns = 0; columns < squaresPerRow; columns++){
+    for (rows = 0; rows < grid.length; rows++){
+        for (columns = 0; columns < grid[rows].length; columns++){
             var node = grid[rows][columns];
-            if (rows - 1 > 0){
+            if (rows - 1 >= 0){
                 node.addNeighbour(grid[rows - 1][columns]);
             }
-            if (columns - 1 > 0){
+            if (columns - 1 >= 0){
+                node2 = 
                 node.addNeighbour(grid[rows][columns - 1]);
             }
-            if (rows + 1 < amountSquareRows){
+            if (rows + 1 < grid.length){
                 node.addNeighbour(grid[rows + 1][columns]);
             }
-            if (columns + 1 < squaresPerRow){
+            if (columns + 1 < grid[rows].length){
                 node.addNeighbour(grid[rows][columns + 1]);
             }
         }
     }
-
 }
+
 
 
 
@@ -150,16 +272,164 @@ function draw(){
 
 function changeGridSize(multiplier){
     squareWidth = multiplier * (defaultSquareWidth) + (multiplier - 1) * squareSpacing;
-    canvas.width = getDesiredCanvasDimension(canvas.width);
-    canvas.height = getDesiredCanvasDimension(canvas.height);
+    canvas.width = getDesiredCanvasDimension(originalCanvasWidth);
+    canvas.height = getDesiredCanvasDimension(originalCanvasHeight);
     setupGrid();
 }
 
 setupGrid();
 draw();
 
+//-------------------------------------------------------------------algorithms 
+queue = []
+finishNode = null;
+algorithmFinished = false;
+doAlgorithm = false;
+executeAlgorithm = false;
+function startAlgorithm(){
+    setupGraph()
+    queue = []
+    queue.push(startingNode);
+    doAlgorithm = true;
+    executeAlgorithm = true;
+    algorithmFinished = false;
+    executeAlgorithm = true;
+}
+
+function finish(node){
+    algorithmFinished = true;
+    finishNode = node;
+}
+
+function doAlgorithmStepBFS(){
+    if (queue.length > 0){
+        currNode = queue.shift();       //get next node from fifo queue
+
+        
+
+        neighbours = currNode.reachableNodes;
+        for (i = 0; i < neighbours.length; i++){
+            neighbourNode = neighbours[i][0];   //reachable nodes are pair of [node, weight] - weight is irrelevant for BFS
+            if (isToBeExplored(neighbourNode)) {
+
+                if (isGoalNode(neighbourNode)){
+                    //found the goal node, end algorithm
+                    finish(neighbourNode);
+                }
+
+                neighbourNode.setParent(currNode);
+                neighbourNode.color = queuedColor;
+                queue.push(neighbourNode);
+            }
+        }
+        if (currNode.color != startPointColor){
+            currNode.color = visited;
+        }
+    }
+}
+
+
+pq = PriorityQueue();
+function startDijkstra(){
+    setupGraph();
+    pq = PriorityQueue();
+    pq.enqueue(startingNode);
+}
+
+/**
+ *  u:= Knoten in Q mit kleinstem Wert in abstand[]
+ 5          entferne u aus Q                                // für u ist der kürzeste Weg nun bestimmt
+ 6          für jeden Nachbarn v von u:
+ 7              falls v in Q:                            // falls noch nicht berechnet
+ 8                 distanz_update(u,v,abstand[],vorgänger[])   // prüfe Abstand vom Startknoten zu v 
+ 9      return vorgänger[]
+ */
+function doAlgorithmStepDijkstra(){
+    if (!pq.isEmpty()){
+        currNode = pq.getMinPrio();
+        neighbours = currNode.getReachableNodes();
+        for (i = 0; i < neighbours.length; i++){
+            neighbourNode = neighbours[i][0];   //reachable nodes are pair of [node, weight]
+            weightToNeighbour = neighbours[i][1];
+            if (exploreNode(neighbourNode)) {    //node should be looked at
+
+                //if we find a queued node, we check if the distance is now cheaper, if yes, we update it.
+                if (neighbourNode.color == queuedColor && currNode.calculateWeight(currNode, neighbourNode) < pq.getPrio(neighbourNode)){
+                    neighbourNode.parent = currNode;
+                    pq.changePriority(neighbourNode, currNode.calculateWeight(currNode, neighbourNode));
+                }
+                
+                if (isGoalNode(neighbourNode)){
+                    //found the goal node, end algorithm
+                    finish(neighbourNode);
+                }
+
+                neighbourNode.setParent(currNode);
+                neighbourNode.color = queuedColor;
+                pq.enqueue(neighbourNode, weightToNeighbour);
+            }
+        }
+        if (currNode.color != startPointColor){
+            currNode.color = visited;
+        }
+    }
+}
+
+function exploreNode(node){
+    if (node.color == visited || node.color == startPointColor || node.color == obstacleColor){
+        return false;
+    }
+    return true;
+}
+
+function isGoalNode(node){
+    return node.color == finishPointColor;}
+
+function isToBeExplored(node){
+    if (node.color == defaultNodeColor || node.color == finishPointColor){
+        return true;
+    }    
+    return false;
+}
+function isVisited(node){
+    if (node.color == visited || node.color == startPointColor){return true;}
+    return false;}
+
+function inQueue(node){
+    return node.color == queuedColor;}
+
+function isGoalNode(node){
+    return node.color == finishPointColor;}
+
+function drawPathBackwards(){
+    if (finishNode != null){
+        finishNode.color = pathColor;
+        finishNode = finishNode.parent;
+    }
+}
+
 
 //-------------------------------------------------------------------Drawing mode handling 
+
+
+/**
+ * defines the drawing loop
+ */
+var drawingIntevalID = setInterval(function(){
+    draw();
+    //doAlgorithm is entered as soon as the algroithm starts
+    //executeAlgorithm is true if another step of the algorithm is supposed to be executed
+    if (doAlgorithm && executeAlgorithm){
+        if (!algorithmFinished){
+            doAlgorithmStepBFS();
+        } else{
+            drawPathBackwards();
+        }
+    }
+}, msBetweenDraws);
+
+
+
 
 const DrawingMode = {
     NoDrawing : -1, 
@@ -174,12 +444,90 @@ const defaultActiveID = '#draw';
 var currentDrawingMode = defaultDrawingMode;
 var currentActiveID = defaultActiveID;
 
-//--------------------------------------------------------------------Listener 
-document.querySelector('#squareSizeSlider').addEventListener('change', function(){
-    changeGridSize(parseInt(document.querySelector('#squareSizeSlider').value, 10));
-    setupGrid();
+
+
+//------------------------------------------------------------------------------------Event handling functions 
+
+function setStartingPosition(coordinates){
+    if (startingNode != null){
+        startingNode.color = defaultNodeColor;
+    }
+    startingPoint = coordinates;
+    startingNode = grid[coordinates.y][coordinates.x];
+    startingNode.color = startPointColor;
+}
+function setFinishingPosition(coordinates){
+    if (goalNode != null){
+        goalNode.color = defaultNodeColor;
+    }
+    finishingPoint = coordinates;
+    goalNode = grid[coordinates.y][coordinates.x];
+    goalNode.color = finishPointColor;
+}
+
+function getMousePositionOnCanvas(event){
+    var rect = canvas.getBoundingClientRect(), // abs. size of element
+    scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for x
+    scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for y
+
+    return {
+        x: (event.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+        y: (event.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
+}
+
+function mapMouseCoordinateToGridSquare(x, y){
+    squareColumn = Math.floor(x / (squareWidth + squareSpacing))
+    squareRow = Math.floor(y / (squareWidth + squareSpacing))
+    return new Coordinate(squareColumn, squareRow);
+}
+
+/*
+var startingPoint = new Coordinate(-1, -1);
+var finishingPoint = new Coordinate(-1, -1);
+
+var startingNode = null;
+var goalNode = null;
+*/
+
+
+function mouseDownOnCanvas(event){
+    try{
+        position = getMousePositionOnCanvas(event);
+        c = mapMouseCoordinateToGridSquare(position.x, position.y);
+        node = grid[c.getY()][c.getX()];
+        //alert(squareIndexes[0] + " " + squareIndexes[1]);
+    
+        switch(currentDrawingMode){
+            case DrawingMode.SetStartingPoint:
+                setStartingPosition(c);
+                break;
+            
+            case DrawingMode.SetFinishingPoint:
+                setFinishingPosition(c);
+                break;
+    
+            case DrawingMode.Erase:
+                node.setColor(defaultNodeColor);
+                break;
+    
+            case DrawingMode.Draw:
+                node.setColor(drawingColor);
+                break;
+    
+            case DrawingMode.LineMode:
+                alert("Not implemented yet! Continue  function mouseDownOnCanvas(event).");
+                break;
+        }
+    } catch(error){
+        console.error(error);
+    }
+
+
     draw();
-}, false);
+}
+
+
 
 //change the drawing mode and set all button-backgrounds to white
 function changeDrawingMode(newDrawingMode, id){
@@ -188,6 +536,16 @@ function changeDrawingMode(newDrawingMode, id){
     currentActiveID = id;
     document.querySelector(currentActiveID).style.color = "green";
 }
+
+//------------------------------------------------------------------------------------------Listener 
+
+document.querySelector('#squareSizeSlider').addEventListener('change', function(){
+    changeGridSize(parseInt(document.querySelector('#squareSizeSlider').value, 10));
+    setupGrid();
+    draw();
+}, false);
+
+
 
 //set the starting button
 document.querySelector('#setStarting').onclick = function(){
@@ -209,11 +567,35 @@ document.querySelector('#draw').onclick = function(){
     currentDrawingMode = DrawingMode.Draw;
 };
 
-
 //calculate algorithm
 document.querySelector('#calculate').onclick = function(){
+    startAlgorithm();
 
-    doAlgorithm = true;
-    executeAlgorithm = true;
     //see way down
+}
+
+
+document.querySelector('#clear').onclick = function(){
+    setupGrid();
+    doAlgorithm = false;
+    resetAlgorithm();
+}
+
+document.querySelector('#stopAlgorithm').onclick = function() {
+    console.log('Stopped Algorithm');
+    executeAlgorithm = false;
+}
+
+document.querySelector('#resumeAlgorithm').onclick = function(){
+    console.log('Resumed Algorithm');
+    executeAlgorithm = true;
+}
+
+
+//aCOLOR PICKER 
+document.querySelector('#drawingColor').addEventListener("change", watchColorPicker, false);
+
+function watchColorPicker(event) {
+    drawingColor = event.target.value;
+    console.log("Drawing color selected : " + drawingColor);
 }
